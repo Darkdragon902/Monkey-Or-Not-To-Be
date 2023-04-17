d840d1c26da595ebaa4acfb2bf33129f577c3268
boolayon <bryanhahuy@gmail.com>
2023-04-16 20:47:26 -0400

    implemented boilerplate mern serverside, with a template login password? not sure didn't test yet

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
-require('dotenv').config();
-
 const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
+
+require('./config/environment');
+require('./database');
+
+const routes = require('./routes/index');
 
 const app = express();
 
