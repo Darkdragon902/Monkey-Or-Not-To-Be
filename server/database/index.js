66e1a1446e2cf5642e6f6c07217f47622d44512d
Michael Regis <93445606+RegisM201@users.noreply.github.com>
2023-04-23 16:44:01 -0400

    Update index.js

--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const options = {};
 
-const database = mongoose.connect(process.env.'mongodb+srv://RegisM:LargeGroup6@cluster0.gmxoq2i.mongodb.net/?retryWrites=true&w=majority', options)
+const database = mongoose.connect(process.env.DATABASE_URL, options)
   .then(() => console.log('Connected to database.'))
   .catch(err => console.error('Error connecting to database:', err.message));
 
