c80d93baf2cfd39c7eeb56e7962d6021ffb2bdf4
trevorgeiger <48469823+trevorgeiger@users.noreply.github.com>
2023-04-28 18:40:59 -0400

    Added basic validation and UX improvements

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -4,10 +4,35 @@ import './Login.css';
 export const Login = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(username, password);
+
+        // Clear previous error
+        setError(null);
+
+        // Basic validation
+        if (!username || !password) {
+            setError('Please fill in both the username and password fields.');
+            return;
+        }
+
+        // Send a request to the server here. This is a placeholder.
+        try {
+            // const response = await fetch('/api/login', { method: 'POST', body: JSON.stringify({ username, password }) });
+            // const data = await response.json();
+
+            // if (!response.ok) {
+            //     throw new Error(data.message || 'Something went wrong.');
+            // }
+
+            // User is now logged in
+            console.log('User logged in:', username);
+        } catch (err) {
+            // If login failed, display an error message
+            setError(err.message);
+        }
     }
 
     return (
@@ -18,6 +43,7 @@ export const Login = (props) => {
                 <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" id="username" name="username" />
                 <label htmlFor="password">Password</label>
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" />
+                {error && <p className="error-message" style={{color: 'red'}}>{error}</p>}
                 <button type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
