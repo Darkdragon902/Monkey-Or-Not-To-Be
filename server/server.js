1fb5ef3b8e8d21666ebe4f231cc17abab36aae6f
boolayon <bryanhahuy@gmail.com>
2023-04-28 13:51:55 -0400

    Added test and passport for authentication, the next thing needed is to hookup user authentication to frontend

--- server/server.js
+++ server/server.js
@@ -6,8 +6,17 @@ require('./config/environment');
 require('./database');
 
 const routes = require('./routes/index');
+const configPassport  = require('./passport/config');
 
 const app = express();
+const assetFolder  = path.resolve(__dirname, '../dist/');
 
 const PORT = process.env.PORT;
+
+app.use(express.static(assetFolder));
+app.use(bodyParser.json());
+
+configPassport(app, express);
+
+app.use('/', routes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
